fdcca66f5efa1b835cb5b8b6be24ef38c31d5c57
KerilDoter <55097756+KerilDoter@users.noreply.github.com>
2022-12-12 14:10:58 +0300

    Add files via upload

--- js/timer.js
+++ js/timer.js
@@ -6,9 +6,9 @@ const btnStop2 = document.querySelectorAll('.btn-stop')[0]
 const btnReset = document.querySelectorAll('.btn-reset')[0]
 const btnReset2 = document.querySelectorAll('.btn-reset')[1]
 const pomidoroCount = document.querySelector('.pomidoro-count')
-let sec = 5
+let sec = 0
 let hrs = 0
-let min = 0
+let min = 25
 let sec2 = 0
 let hrs2 = 0
 let min2 = 10
@@ -30,31 +30,31 @@ function tick(){
         }
     }
     if (sec == 0 & min == 0 & hrs == 0){
-        min = 0; sec = 2; hrs = 0 // первый помидор окончен
+        min = 5; sec = 0; hrs = 0 // первый помидор окончен
         pomidor = pomidor + 1
         pomidoroCount.textContent = "Отдых"
         console.log(pomidor);
         if (pomidor == 2){
             pomidoroCount.textContent = "Второй помидор"
-            min = 0; sec = 5; hrs = 0 // второй помидор начат
+            min = 25; sec = 0; hrs = 0 // второй помидор начат
         }
         if (pomidor == 3){
-            min = 0; sec = 2; hrs = 0 // второй помидор закончен
+            min = 5; sec = 0; hrs = 0 // второй помидор закончен
         }
         if (pomidor == 4){
             pomidoroCount.textContent = "Третий помидор"
-            min = 0; sec = 5; hrs = 0 // третий помидор начат
+            min = 25; sec = 0; hrs = 0 // третий помидор начат
         }
         if (pomidor == 5){
-            min = 0; sec = 2; hrs = 0 // третий помидор окончен
+            min = 5; sec = 0; hrs = 0 // третий помидор окончен
         }
         if (pomidor == 6){
             pomidoroCount.textContent = "Четвертый помидор"
-            min = 0; sec = 5; hrs = 0 // четвертый помидор начат
+            min = 25; sec = 0; hrs = 0 // четвертый помидор начат
         }
         if (pomidor == 7){
             timeShow.textContent = "00:25:00"
-            sec = 5; min = 0; hrs = 0
+            min = 25; sec = 0; hrs = 0
             clearTimeout(t)
             btnStart.disabled = false
             pomidoroCount.textContent = "Первый помидор"
@@ -102,6 +102,7 @@ btnStart.onclick = function(){
 btnStart2.onclick = function(){
     add2();
     btnStart2.disabled = true
+    btnStop2.disabled = false;
 }
 btnStop2.onclick = function() {
     clearTimeout(t2);
@@ -110,7 +111,7 @@ btnStop2.onclick = function() {
 }
 btnReset.onclick = function(){
     timeShow.textContent = "00:25:00"
-    sec = 5; min = 0; hrs = 0
+    sec = 0; min = 25; hrs = 0
     clearTimeout(t)
     btnStart.disabled = false
     btnReset.disabled = true
